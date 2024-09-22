f82233dd792b310d91e3ff6e57b5b4f94bf54764
yourimmary <151358655+yourimmary@users.noreply.github.com>
2024-09-22 18:51:30 +0900

    #240922 타이틀
    타이틀에서 인게임씬으로 들어갈 때, 연출 작업

--- RoguelikeShootingGame/Assets/2.Scripts/UIs/TitleWindow.cs
+++ RoguelikeShootingGame/Assets/2.Scripts/UIs/TitleWindow.cs
@@ -5,9 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class TitleWindow : MonoBehaviour
 {
+    [SerializeField] Animator _animator;
+
+    AsyncOperation _oper;
+
+    private void Start()
+    {
+        StartCoroutine(ActiveButtons());
+    }
+
     public void ClickStartButton()
     {
-        SceneManager.LoadScene("GameScene");
+        transform.GetChild(0).gameObject.SetActive(false);
+
+        _oper = SceneManager.LoadSceneAsync("GameScene");
+        _oper.allowSceneActivation = false;
+
+        StartCoroutine(LoadGameScene());
     }
 
     public void ClickQuitButton()
@@ -17,5 +31,21 @@ public class TitleWindow : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+
+    IEnumerator ActiveButtons()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    IEnumerator LoadGameScene()
+    {
+        _animator.SetBool("GameStart", true);
+
+        yield return new WaitForSeconds(1.2f);
+
+        _oper.allowSceneActivation = true;
     }
 }
